7181794b346593370087640ad136031bf2100277
Stephen Lam <justslamthings@gmail.com>
2023-02-28 18:49:53 -0500

    Calender shows up after installing Mobiscroll

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -2,11 +2,25 @@ import './App.css';
 import NavigationBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import { Eventcalendar } from "@mobiscroll/react";
+import "@mobiscroll/react/dist/css/mobiscroll.min.css";
+
 function App() {
   return (
     <>
       <p>Hello testing</p>
       <NavigationBar />
+
+      <Eventcalendar 
+    data={[{
+        start: new Date(),
+        title: 'Today\'s event'
+    }, {
+        start: new Date(2020, 11, 18, 9, 0),
+        end: new Date(2020, 11, 20, 13, 0),
+        title: 'Multi day event'
+    }]}
+/>
     </>
   );
 }
