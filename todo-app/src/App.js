f763023e1441c8f3a33509e1d55b063c9f1c8f03
jwong65 <jason.wong6507@gmail.com>
2023-02-28 20:10:10 -0500

    Changed the App.js to have the component

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,28 +1,16 @@
 import './App.css';
 import NavigationBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Home from './pages/Home'
 
-import { Eventcalendar } from "@mobiscroll/react";
-import "@mobiscroll/react/dist/css/mobiscroll.min.css";
 
 function App() {
   return (
     <>
       <p>Hello testing</p>
       <NavigationBar />
+      <Home />
 
-      <Eventcalendar 
-    data={[{
-        start: new Date(),
-        title: 'Today\'s event'
-    }
-    // , {
-    //     start: new Date(2020, 11, 18, 9, 0),
-    //     end: new Date(2020, 11, 20, 13, 0),
-    //     title: 'Multi day event'
-    // }
-    ]}
-/>
     </>
   );
 }
