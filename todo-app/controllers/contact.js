48c5ee0dcbe0774ec2fa69455f70a99ce0f17313
Adeshvir Dhanoa <ad13dhanoa@gmail.com>
2023-03-02 13:32:57 -0500

    finished form with useState, added css, made contact us schema, and started decalrations for contact controller

---
+++ todo-app/controllers/contact.js
@@ -0,0 +1,6 @@
+const {Contacts} = require('../models/contactUs');
+const express = require('express');
+const router = express.Router();
+
+
+module.exports = router;
--- todo-app/src/pages/AddEvent.js
+++ todo-app/src/pages/AddEvent.js
@@ -1,65 +1,62 @@
 import React from 'react'
 import { useState } from 'react';
+import NavBar from '../components/NavBar';
+import '../form.css'
 
 export default function AddEvent() {
   const [title, setTitle] = useState('');
   const [date, setDate] = useState('');
   const [startTime, setStartTime] = useState('');
   const [endTime, setEndTime] = useState('');
+  const [category, setCategory] = useState('')
   const [note, setNote] = useState('');
-  const [selectedOption, setSelectedOption] = useState('');
 
-  const options = [
-    { value: 'option1', label: 'Family' },
-    { value: 'option2', label: 'Career' },
-    { value: 'option3', label: 'School' },
-    { value: 'option4', label: 'Interpersonal' },
-    { value: 'option5', label: 'Growth' },
-  ];
-
-  const handleOptionChange = (event) => {
-    setSelectedOption(event.target.value);
-  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
   }
   return (
+    <>
+    <NavBar/>
+    <h1>Add Event Here:</h1>
 <form onSubmit={handleSubmit}>
       <label>
         Title:
         <input type="text" value={title} onChange={(event) => setTitle(event.target.value)} />
       </label>
+      <br/>
       <label>
         Date:
         <input type="date" value={date} onChange={(event) => setDate(event.target.value)} />
       </label>
+      <br/>
       <label>
         Start Time:
         <input type="time" value={startTime} onChange={(event) => setStartTime(event.target.value)} />
       </label>
+      <br/>
       <label>
         End Time:
         <input type="time" value={endTime} onChange={(event) => setEndTime(event.target.value)} />
       </label>
-      {options.map((option) => (
-        <div key={option.value}>
-          <label>
-            <input
-              type="radio"
-              value={option.value}
-              checked={selectedOption === option.value}
-              onChange={handleOptionChange}
-            />
-            {option.label}
-          </label>
-        </div>
-      ))}
+      <br/>
+      <label>
+        Category:
+        <select value={category} onChange={(event) => setCategory(event.target.value)}>
+          <option value="category1">Personal</option>
+          <option value="category2">Family</option>
+          <option value="category3">Career</option>
+          <option value="category4">School</option>
+        </select>
+      </label>
+      <br/>
       <label>
         Note:
-        <textarea value={note} onChange={(event) => setNote(event.target.value)} />
+        <input type = 'text' value={note} onChange={(event) => setNote(event.target.value)} />
       </label>
-      <button type="submit">Submit</button>
+      <br/>
+      <button className = 'submitButton' type="submit">Submit</button>
     </form>
+</>
   );
 }
