44d4802ae09cf1bc6e3eb8afab8ef5822634fc30
jwong65 <jason.wong6507@gmail.com>
2023-03-02 19:30:51 -0500

    Removed comments

--- todo-app/src/pages/Home.js
+++ todo-app/src/pages/Home.js
@@ -45,13 +45,6 @@ export default function Home() {
           start: new Date(),
           title: 'Today\'s event'
       }
-      // ,
-      
-      //  {
-      //     start: new Date(2020, 11, 18, 9, 0),
-      //     end: new Date(2020, 11, 20, 13, 0),
-      //     title: 'Multi day event'
-      // }
       ]}
       
   />
