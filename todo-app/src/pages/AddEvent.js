85d8eb3e239940d53e84eb25c59a46aca18f9b82
Diego Guardiola <diego.guardiola@falcoautomation.local>
2023-03-02 20:56:57 -0500

    connecting form to mongo

--- todo-app/src/pages/AddEvent.js
+++ todo-app/src/pages/AddEvent.js
@@ -1,48 +1,67 @@
+import axios from 'axios'
 import React from 'react'
 import { useState } from 'react';
 import NavBar from '../components/NavBar';
 import '../form.css'
-
-export default function AddEvent() {
-  const [title, setTitle] = useState('');
-  const [date, setDate] = useState('');
-  const [startTime, setStartTime] = useState('');
-  const [endTime, setEndTime] = useState('');
-  const [category, setCategory] = useState('')
-  const [note, setNote] = useState('');
+import { useNavigate } from 'react-router-dom'
 
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+function AddEvent() {
+
+const navigate = useNavigate()
+
+  const [formData, setFormData] = useState({
+    title: '',
+    startTime: '',
+    endTime: '',
+    category: '',
+    note: '',
+  })
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    const newFormDocument = {
+      title: formData.title,
+      startTime: formData.startTime,
+      endTime: formData.endTime,
+      category: formData.category,
+      note: formData.note
+    }
+
+    try {
+      await axios.post('/TODO/v1/myforms', newFormDocument)
+      alert('Success')
+    } catch {
+      console.log('Error')
+    }
+
+    navigate('/')
   }
+
   return (
     <>
     <NavBar/>
     <h1>Add Event Here:</h1>
-<form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit}>
       <label>
         Title:
-        <input type="text" value={title} onChange={(event) => setTitle(event.target.value)} />
+        <input type="text" value={formData.title} onChange={(e) => setFormData({ ...formData, title: e.target.value})} />
       </label>
       <br/>
       <label>
-        Date:
-        <input type="date" value={date} onChange={(event) => setDate(event.target.value)} />
-      </label>
-      <br/>
-      <label>
-        Start Time:
-        <input type="time" value={startTime} onChange={(event) => setStartTime(event.target.value)} />
+        Start Time
+        <input type="text" value={formData.startTime} onChange={(e) => setFormData({ ...formData, startTime: e.target.value})} />
       </label>
       <br/>
       <label>
         End Time:
-        <input type="time" value={endTime} onChange={(event) => setEndTime(event.target.value)} />
+        <input type="text" value={formData.endTime} onChange={(e) => setFormData({ ...formData, endTime: e.target.value})} />
       </label>
       <br/>
       <label>
         Category:
-        <select value={category} onChange={(event) => setCategory(event.target.value)}>
+        <select value={formData.category} onChange={(e) => setFormData({ ...formData, category: e.target.value})}>
           <option value="category1">Personal</option>
           <option value="category2">Family</option>
           <option value="category3">Career</option>
@@ -52,11 +71,13 @@ export default function AddEvent() {
       <br/>
       <label>
         Note:
-        <input type = 'text' value={note} onChange={(event) => setNote(event.target.value)} />
+        <input type="text" value={formData.note} onChange={(e) => setFormData({ ...formData, note: e.target.value})} />
       </label>
       <br/>
       <button className = 'submitButton' type="submit">Submit</button>
     </form>
-</>
+  </>
   );
 }
+
+export default AddEvent()
