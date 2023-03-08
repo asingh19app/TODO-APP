db07bdbf8b6d0b8b24c37d78ca2e42023b3572c1
jwong65 <jason.wong6507@gmail.com>
2023-03-07 19:47:00 -0500

    Added page delete

---
+++ todo-app/src/pages/DeleteEvent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Index from '../components/Index'
+import NavBar from '../components/NavBar'
+
+export default function DeleteEvent() {
+  return (
+  <>
+  <NavBar/>
+        <div>
+            <h1>Delete Events</h1>
+        </div>
+</>
+  )
+}
