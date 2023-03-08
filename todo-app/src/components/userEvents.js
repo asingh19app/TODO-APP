08fce84301b7574405da4f627e4179d156665b29
JustSlamThings <justslamthings@gmail.com>
2023-03-07 19:10:17 -0500

    separating defaultEvents into userEvent.js

---
+++ todo-app/src/components/userEvents.js
@@ -0,0 +1,11 @@
+let userEvents;
+// const defaultEvents = [{
+//     id: 1,
+//     start: '2023-03-08T13:00',
+//     end: '2023-03-08T13:45',
+//     title: 'Lunch @ Butcher\'s',
+//     description: '',
+//     allDay: false,
+//     free: true,
+//     color: '#009788'
+// },
