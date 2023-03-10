b4a7c542271cc628d1260ce3e1fa91c983b90dbc
jwong65 <jason.wong6507@gmail.com>
2023-03-09 20:04:57 -0500

    Revert "Added the comments and issues with handleDelete. Changed the onChange and onSubmit for the form"
    This reverts commit 43a9678da3182ba6f285465c5a0416908654503d.

--- todo-app/src/pages/DeleteEvent.js
+++ todo-app/src/pages/DeleteEvent.js
@@ -18,31 +18,15 @@ export default function DeleteEvent() {
     }
     fetchData();
   }, []);
-  // async function handleDelete() {
-  //   try {
-  //     await axios.delete(`http://localhost:5000/TODO/v1/form/:${selectedOption._id}`); 
-  //     setMyOptions(myOptions.filter(obj => obj._id !== selectedOption._id));
-  //     setSelectedOption('');
-  //   } catch (error) {
-  //     console.error(error);
 
-  // There's an issue where if you try to delete by selecting an option I can't get it to delete the right one. It only deletes what's the first in the array
-  // I tried to fix it by creating the function changedOption - it would change the useState of selectedOption onChange of the select form.
-  // The problem I found out is that ${selectedOption._id} is not working. It's giving an undefined.
   async function handleDelete() {
     try {
-      console.log(`http://localhost:5000/TODO/v1/form/:${selectedOption._id}`)
-      setMyOptions(myOptions.filter(obj => obj._id !== selectedOption._id))
+      setMyOptions(myOptions.filter(obj => obj._id !== selectedOption._id));
       await axios.delete(`http://localhost:5000/TODO/v1/form/:${selectedOption._id}`); 
-     } catch (error) {
+      setSelectedOption('');
+    } catch (error) {
       console.error(error);
     }
-  }
-
-  function changedOption(e) {
-    setSelectedOption(e.target.value);
-    console.log(e.target.value)
-    console.log(selectedOption.title)
   }
 
   return (
@@ -52,7 +36,7 @@ export default function DeleteEvent() {
               <h1>Delete Events</h1>
               <form onSubmit={handleDelete}>
                 <div>
-                  <select onChange={e=>changedOption(e)}>
+                  <select>
                     {myOptions.map(obj => (
                       <option key={obj._id} value={obj.title}>{obj.title}</option>
                     ))}
